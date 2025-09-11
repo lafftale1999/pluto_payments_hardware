ef2eadde4b2c93b455f16d7b89fee9cbc1288ec9
Lafftale <lafftale1999@gmail.com>
2025-09-11 08:49:09 +0200

    wifi created

---
+++ main/include/wifi_implementation.h
@@ -0,0 +1,13 @@
+#ifndef WIFI_IMPLEMENTATION_H
+#define WIFI_IMPLEMENTATION_H
+
+#include "esp_err.h"
+#include <stdbool.h>
+
+#define WIFI_MAX_WAIT_MS    5000
+
+esp_err_t wifi_init();
+bool wifi_is_connected();
+esp_err_t wait_for_connection(void);
+
+#endif
